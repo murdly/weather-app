6674f435a1accb6d6601acf8342c82fde8e7c54a
kanko <fretowski@gmail.com>
2015-10-27 08:52:08 +0100

    add api key

--- app/src/main/java/com/example/arkadiuszkarbowy/weatherapp/rest/service/ApiService.java
+++ app/src/main/java/com/example/arkadiuszkarbowy/weatherapp/rest/service/ApiService.java
@@ -5,6 +5,7 @@ import com.example.arkadiuszkarbowy.weatherapp.rest.model.Weather;
 
 import retrofit.Call;
 import retrofit.http.GET;
+import retrofit.http.Headers;
 import retrofit.http.Path;
 import retrofit.http.Query;
 
@@ -14,9 +15,11 @@ import retrofit.http.Query;
 public interface ApiService {
      String UNITS = "metric";
 
+     @Headers("x-api-key:b21242744b05bf6ef7aa2fe19a67beb3")
      @GET("/data/2.5/weather")
      Call<Weather> weather(@Query("id") String id, @Query("units") String units);
 
+     @Headers("x-api-key:b21242744b05bf6ef7aa2fe19a67beb3")
      @GET("data/2.5/forecast")
      Call<Forecast> forecast(@Query("id") String id, @Query("units") String units);
 }
